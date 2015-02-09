d9dfd1c8cd345f5b558a0b71dff8c94578f7a3bc
georgekyrg <georgekyrg@gmail.com>
2015-02-09 16:10:47 +0200

    finish & demo

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,6 +1,6 @@
   var templateItem = Hogan.compile(
   	'<div class="hit">' +
-  		'<img class="image-attribute" src={{edmPreview}}/>'+
+  		'<img class="image-attribute" src="{{edmPreview}}"/>'+
   		'<span class="primary-attribute">'+
   			'<a href="{{guid}}">{{title}}</a>'+
   		'</span>'+
@@ -9,7 +9,7 @@
   	'</div>');
 
 	$(document).ready(function() {
-	  var algolia = new AlgoliaSearch("2SMV2HKN8F", "56fe227808d599d13a268d171e6b2dc8");
+	  var algolia = new AlgoliaSearch('2SMV2HKN8F', '56fe227808d599d13a268d171e6b2dc8');
 	  var index = algolia.initIndex('Europeana');
 
 	  $('#typeahead-algolia').typeahead(null, {
@@ -17,11 +17,38 @@
 	    displayKey: 'title', // attribute used for display
 	    templates: {
 	      suggestion: function(hit) {
+	      	console.log(hit);
+	      	console.log("and after");
+	      	if (hit.edmPreview === undefined){
+					      		
+	      		//TEXT, VIDEO, SOUND, IMAGE, 3D    	
+	      		switch(hit.type) {
+				    case 'TEXT':
+				        hit.edmPreview = ['http://europeanastatic.eu/api/image?size=FULL_DOC&type=TEXT'];
+				        break;
+				    case 'VIDEO':
+				        hit.edmPreview = ['http://europeanastatic.eu/api/image?size=FULL_DOC&type=VIDEO'];
+				        break;
+				    case 'SOUND':
+				    	hit.edmPreview = ['http://europeanastatic.eu/api/image?size=FULL_DOC&type=SOUND'];
+				        break;
+				    case 'IMAGE':
+				    	hit.edmPreview = ["http://europeanastatic.eu/api/image?size=FULL_DOC&type=IMAGE"];
+				        break;
+				    case '3D':
+				    	hit.edmPreview = ["http://europeanastatic.eu/api/image?size=FULL_DOC&type=3D"];
+				        break;
+				    default:
+				        hit.edmPreview = ["http://europeanastatic.eu/api/image?size=FULL_DOC&type=TEXT"];
+				        break;
+				}
+		
+	      	} 
 	      	return templateItem.render(hit);
 	      }
 	    }
 	  }).on('typeahead:selected', function(event, suggestion, dataset) {
-	    if ('guid' !== '') {
+	    if (suggestion['guid'] !== '') {
 	      chrome.tabs.create({url: suggestion['guid']});
 	    }
 	  }).focus();
